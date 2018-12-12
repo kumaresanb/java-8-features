c98ad1d782a2fac760b6fe3cf179d69b34b229bf
Kumaresan BK <EVOLVUS\kumaresanb@ELPBLR-035.evolvus.com>
2018-12-12 17:37:50 +0530

    java basic

---
+++ src/main/java/com/evolvus/java/practies/java8features/Manager.java
@@ -0,0 +1,41 @@
+package com.evolvus.java.practies.java8features;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Manager {
+	public static void main(String[] args) {
+		List<Integer> myList=new ArrayList<Integer>();
+		for (int i = 0; i < 10; i++) myList.add(i);
+		
+		Iterator<Integer> iterator=myList.iterator();
+		while (iterator.hasNext()) {
+			System.out.println("Ierator value::"+iterator.next());
+			
+		}
+		
+		myList.forEach(new Consumer<Integer>() {
+
+			public void accept(Integer t) {
+					System.out.println("forEach anonymous class values::"+t);
+			}
+			
+		});
+		
+		MyConsumer myConsumer=new MyConsumer();
+		myList.forEach(myConsumer);
+				
+	
+	}
+
+}
+
+class MyConsumer implements Consumer<Integer>{
+
+	public void accept(Integer t) {
+		System.out.println("myConsumer class values::"+t);
+	}
+	
+}
